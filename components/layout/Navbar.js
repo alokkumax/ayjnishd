49254c7dcbe2526d280130e10c362663bb40e6b3
Abhilashax <abhinanda361@gmail.com>
2022-08-10 23:11:32 +0530

    Developed Navbar

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,46 +1,105 @@
 import React from 'react'
-// import logo from ".../images/logo-sih.png"
+import Image from 'next/image'
+import logo from "../../images/logo-sih.png"
 // import bootstrap from 'bootstrap'
+
+import { AiOutlineSound } from 'react-icons/ai';
+import { HiMenu } from 'react-icons/hi';
 
 export default function Navbar() {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg ">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">AYJNISHD</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+  <Image src={logo} width={120} height={50}/>
+    <h1 className="navbar-brand mx-3 text-dark" href="#">AYJNISHD</h1>
+    <button className="navbar-toggler text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      {/* <span className="navbar-toggler-icon"></span> */}
+      <HiMenu  className="navbar-toggler-icon"/>
     </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className=" collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 justify-content-end ">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">About
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">ADIP Scheme</a></li>
+            <li><a className="dropdown-item" href="#">Guidance And Support</a></li>
+            <li><a className="dropdown-item" href="#">Institute </a></li>
+            <li><a className="dropdown-item" href="#">Regional Centres </a></li>
+            <li><a className="dropdown-item" href="#">Copyright Scheme</a></li>
+            
+          </ul>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            About
+            Apply
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Scheme</a></li>
-            <li><a className="dropdown-item" href="#">Institute </a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Regional Centres </a></li>
-            <li><a className="dropdown-item" href="#">Guidance And Support</a></li>
-            <li><a className="dropdown-item" href="#">Copyright Scheme</a></li>
-            <li><a className="dropdown-item" href="#">ADIP Scheme</a></li>
+            <li><a className="dropdown-item" href="#">CI Candidacy</a></li>
+            <li><a className="dropdown-item" href="#">How to Apply </a></li>
+            {/* <li><hr className="dropdown-divider"/></li> */}
+            <li><a className="dropdown-item" href="#">CI Surgery Enrollment </a></li>
+            <li><a className="dropdown-item" href="#">Pre &amp; POst CI Surgery</a></li>
+            <li><a className="dropdown-item" href="#">CI Processor Manual</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Login
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Request for Audiologist</a></li>
+            <li><a className="dropdown-item" href="#">Empanallment of Hospitals </a></li>
+            <li><a className="dropdown-item" href="#">Surgical Report</a></li>
+            <li><a className="dropdown-item" href="#">Activation &amp; Mapping</a></li>
+            <li><a className="dropdown-item" href="#">Check Application Status</a></li>
+            <li><a className="dropdown-item" href="#">Check Application Details</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contact
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Contact Us</a></li>
+            <li><a className="dropdown-item" href="#">Feedback</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Others
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Report of Events</a></li>
+            <li><a className="dropdown-item" href="#">Duration of Therapy</a></li>
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Languages
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Hindi</a></li>
+            <li><a className="dropdown-item" href="#">Marathi</a></li>
+            <li><a className="dropdown-item" href="#">Telugu</a></li>
+            <li><a className="dropdown-item" href="#">Tamil</a></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <a className="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <AiOutlineSound/>
+          </a>
         </li>
+        
       </ul>
-      <form className="d-flex" role="search">
+      {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      </form> */}
     </div>
   </div>
 </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-
+import style from "../stylesheet/style.css"
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
