a342dbe7c6aec13ac84f1be9e042f5e798c9c937
Abhilashax <abhinanda361@gmail.com>
2022-08-12 16:44:37 +0530

    Developed Hero Section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from "../components/layout/Navbar"
+import Hero from "../components/layout/Hero"
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 } 
