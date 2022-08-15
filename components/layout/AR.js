f1d90fcbbbe483458326985c77b2a745caa14122
KeshavA <agarwalkeshav8399@gmail.com>
2022-08-15 19:07:14 +0530

    developed 3d model

---
+++ components/layout/AR.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "@google/model-viewer";
+
+export default function ar() {
+  return (
+    <div>        <model-viewer src="/earpiece.glb" camera-controls auto-rotate ar ios-src="/earpiece.usdz"></model-viewer>
+    </div>
+  )
+}
--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,8 @@ class MyDocument extends Document {
         <Head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
         <script async src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossOrigin="anonymous"></script>
+        {/* <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script> */}
+        
         </Head>
         <body>
           <Main />
