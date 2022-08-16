01212f0bee088cf86038ba2103098d8110b655cf
alokkumax <alokkumax1@gmail.com>
2022-08-16 22:29:41 +0530

    added page

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/layout/Navbar'
+import SideBar from '../components/layout/SideBar'
 
 export default function aboutPage() {
   return (
     <div>
     aboutPage
+    <SideBar/>
     </div>
   )
 }
