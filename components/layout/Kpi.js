b8640c9093406d1f09f1d93cc813e376269e7edb
alokkumax <alokkumax1@gmail.com>
2022-08-19 19:46:38 +0530

    fixed map

--- components/layout/Kpi.js
+++ components/layout/Kpi.js
@@ -10,14 +10,14 @@ export default function Kpi({ data }) {
     const [res, setRes] = useState([])
     useEffect(()=>{
         setRes(data)
-    })
+    },[])
   return (
     <div className="container row d-row-center col-12 m-auto">
         <div id="kpi1" className="col-lg-3 col-sm-6 col-12 d-row-center my-3">
             <Image src={Kpi1} alt="" height={80} width={80}></Image>
             <div className='d-col-center kpi1-text'>
-                {res.map(item =>(
-                    <h4>{item.professionals}</h4>
+                {res.map((item,index) =>(
+                    <h4 key={index}>{item.professionals}</h4>
                 ))}
                 <p className=''>Registered for CI</p>
             </div>
@@ -25,8 +25,8 @@ export default function Kpi({ data }) {
         <div id="kpi2" className="col-lg-3 col-sm-6 col-12 d-row-center my-3">
         <Image src={Kpi2} height={80}  alt="" width={80}></Image>
             <div className='d-col-center kpi2-text'>
-            {res.map(item =>(
-                    <h4>{item.registered}</h4>
+            {res.map((item,index) =>(
+                    <h4 key={index}>{item.registered}</h4>
                 ))}
                 <p className=''>Professionals Registered</p>
             </div>
@@ -34,8 +34,8 @@ export default function Kpi({ data }) {
         <div id="kpi3" className="col-lg-3 col-sm-6 col-12 d-row-center my-3">
         <Image src={Kpi3} height={80} alt="" width={80}></Image>
             <div className='d-col-center kpi3-text'>
-            {res.map(item =>(
-                    <h4>{item.surgeries}</h4>
+            {res.map((item,index) =>(
+                    <h4 key={index}>{item.surgeries}</h4>
                 ))}
                 <p className=''>Surgeries Done</p>
             </div>
@@ -43,8 +43,8 @@ export default function Kpi({ data }) {
         <div id="kpi4" className="col-lg-3 col-sm-6 col-12 d-row-center my-3">
         <Image src={Kpi4} height={80} alt="" width={80}></Image>
             <div className='d-col-center kpi4-text'>
-            {res.map(item =>(
-                    <h4>{item.hospitals}</h4>
+            {res.map((item,index) =>(
+                    <h4 key={index}>{item.hospitals}</h4>
                 ))}
                 <p className=''>Hospitals Empanelled</p>
             </div>
