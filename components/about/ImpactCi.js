658202fbb41987db1c9ba045a91e60b7081164d6
alokkumax <alokkumax1@gmail.com>
2022-08-21 11:02:02 +0530

    Made few changes

---
+++ components/about/ImpactCi.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const ImpactCi = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>IMPACT OF CI</h1><br/>
+        <p>
+          Hearing impairment has a significant impact on the child and his
+          family. This impact is seen on every aspect of life including
+          cognitive, communication, psychosocial, educational, and personality
+          development as well as on financial condition of the family.
+          Considering the wide ranging impact of hearing impairment early
+          identification and intervention during the critical learning years,
+          has been found to be very important to minimize the impact of the
+          hearing impairment in the child’s over all development.
+        </p><br/>
+        <p>
+          With the recent advances in hearing technology including cochlear
+          implants, children with severe to profound hearing loss have access to
+          most of the sound of spoken language. Most of children who are born
+          deaf of hard of hearing can learn to listen to their own voices,
+          listen to the voices of others and listen to the sounds of life.
+        </p><br/>
+        <p>
+          It is important to know about impact of cochlear implants in young
+          children as it involves high cost, surgery, and a high level of family
+          expectations. Factors including age at onset of deafness, age at
+          implantation, amount of cochlear implant experience, and educational
+          training determine the overall benefits a child will have post
+          cochlear implantation. Following sections will highlight the various
+          impacts on the children’s lives post implantation of cochlear
+          implants.
+        </p><br/>
+
+        <h3>Auditory skills</h3><br/>
+
+        <p>
+          Children with cochlear implants achieve performance levels that
+          exceeded those of their non-implanted peers who used other sensory
+          aids, including conventional hearing aids and vibrotactile aids.2 The
+          improvement of hearing in terms of purely audiological performance is
+          remarkable: audiological tests including monosyllabic word and
+          sentences tests show a noticeable increase in open word recognition
+          after implantation of cochlear implants3. The improved auditory skills
+          enhance the opportunity for acquisition of speech and language.
+        </p><br/>
+      </div>
+    </>
+  );
+};
+
+export default ImpactCi;
--- components/layout/News.js
+++ components/layout/News.js
@@ -1,28 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-
+import Slider from "react-slick";
+async function fetchNews() {
+  const news = (
+    await axios.get(
+      "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+    )
+  ).data;
+  return news;
+}
 export default function News() {
   const [data , setData] = useState([""]);
   const src = `https://images.livemint.com/img/2022/04/21/600x338/long_covid_symptoms_1650540839356_1650540839488.jpg`;
-  useEffect(()=>{
-    axios.get("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
-    fetch("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
-    .then((res)=>{
-      return res.json();
-    }).then((response)=>{
-      // console.log(response)
-      // console.log(response.articles)
-      const x = response.articles;
-      // console.log(x)
-      setData(x)
-    })
-  },[])
+  useEffect(() => {
+    fetchNews().then((data) => {
+      console.log(data.articles);
+      setData(data.articles);
+    });
+  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const news = data.slice(0, 5).map((n, i) => {
+    return (
+      <div key={i}>
+        <a href={n.url}>
+          <img  src={n.urlToImage} alt="" width={600} height={300} />
+        </a>
+        <p className="text-center mt-2">{n.title}</p>
+        <p className="text-center ">{n.description}</p>
+      </div>
+    );
+  });
+  console.log(news);
   return (
-   <div>
-    <h2 className='text-center p-5'>LATEST NEWS</h2>
-
-    <h2 className='text-center p-5'>READ MORE</h2>
-   </div>
-    
-  )
+    <div>
+      <h2 className="text-center p-5">LATEST NEWS</h2>
+      <Slider className="mb-5" {...settings}>{news}</Slider>
+    </div>
+  );
 }
