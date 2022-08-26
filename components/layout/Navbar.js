a2aa0f5c9892680b7d6fe5dcdb54555486d1a831
Abhilashax <abhinanda361@gmail.com>
2022-08-26 16:06:44 +0530

    Final changes in navigation

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -309,7 +309,7 @@ export default function Navbar(props) {
                     </Link>
                   </li>
                   <li>
-                    <Link href="/others/pre-post">
+                    <Link href="/others/postpre">
                       <a className="dropdown-item" href="#">
                         Pre &amp; Post Surgery
                       </a>
