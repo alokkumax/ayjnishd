a0045dd853ce3493afdde3c0549929bd06815ae7
Abhilashax <abhinanda361@gmail.com>
2022-08-26 16:18:41 +0530

    Made few changes

--- pages/text-to-speech.js
+++ pages/text-to-speech.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Footer from '../components/layout/Footer'
+import Navbar from '../components/layout/Navbar'
 import Text_to_Speech from "../components/layout/Text-to-Speech"
 export default function text_to_speech() {
   return (
     <div>
+      <Navbar active="Home"/>
         <Text_to_Speech/>
+      
     </div>
   )
 }
