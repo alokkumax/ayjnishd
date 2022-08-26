40030d896b723fec999cd8d570fa1bfe1783677d
alokkumax <alokkumax1@gmail.com>
2022-08-26 09:23:52 +0530

    warns

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -6,14 +6,14 @@ const Login = () => {
       <center>
         <form id="loginformcss">
           <div className="mb-3">
-            <label for="regno" className="form-label">
+            <label htmlFor="regno" className="form-label">
               Unique Registration Number
             </label>
             <input type="number" className="form-control" required />
           </div>
           <div className="mb-3">
-            <label for="name" className="form-label">
-              Applicant's Name
+            <label htmlFor="name" className="form-label">
+              Applicant`&apos;`s Name
             </label>
             <input type="text" className="form-control" required />
           </div>
