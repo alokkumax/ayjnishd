669069b97dc79f261d56974bc337b619bae41142
Ashish Gupta <ashishg0926@gmail.com>
2022-08-26 12:27:31 +0530

    added links in footer

--- components/layout/Text-to-Speech.js
+++ components/layout/Text-to-Speech.js
@@ -39,7 +39,7 @@ const App = () => {
     <div>
       <center>
       <h3 id="mictext"><b>Microphone: {listening ? 'on' : 'off'}</b></h3>
-      <p className='ptransript' id='content'>{transcript}</p>
+      <h6 className='ptransript' id='content'>{transcript}</h6>
       <button onClick={SpeechRecognition.startListening} className="startbut">Start</button>&nbsp;&nbsp;
       <button onClick={SpeechRecognition.stopListening} className="startbut">Stop</button>&nbsp;&nbsp;
       <button onClick={resetTranscript} className="startbut">Reset</button>&nbsp;&nbsp;</center>
