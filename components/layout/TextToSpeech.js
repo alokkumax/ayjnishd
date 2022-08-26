752c48391e908714e608f9ae334c9cda0023d03c
Soumwadeep Guha <soumwadeep@gmail.com>
2022-08-26 15:15:56 +0530

    Added CSS Styling To Hearing,Voice Assist

---
+++ components/layout/TextToSpeech.js
@@ -0,0 +1,56 @@
+
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+import React from 'react';
+import { useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
+// import './style.css';
+ 
+const App = () => {
+  function getResponse(content) {
+    axios.post("https://chatbot-sih-new.herokuapp.com/model/parse", {
+      "text": `${content}`
+    }).then((data)=>{
+      // console.log(data.data);
+      setName(data.data.intent.name)
+    })
+  }
+  const [name, setName] = useState('');
+  
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser does not support speech recognition.</span>;
+  }
+  var response=""
+  var user_said=""
+  if(document.getElementById('content')==null)
+    user_said=""
+  else
+    user_said=document.getElementById('content').innerText;
+  //console.log(user_said);
+ getResponse(user_said);
+ if(name=="ask_cochlear")
+  response="The cochlear implant is a prosthetic device, a part of which is surgically implanted inside the cochlea.\nIt is a device to be used by someone with almost complete hearing loss. While a hearing aid provides amplified sound energy to the ear,\n the cochlear implant directly provides electrical stimulation to the nerve endings in the cochlea."
+ else if(name=="bot_job")
+  response="I am here to help you with your queries. You can ask me about:\n 1. What is cochlear implant?\n 2. How to apply?"
+ 
+  
+  return (
+    <div>
+      <center>
+      <h3 id="mictext"><b>Microphone: {listening ? 'on' : 'off'}</b></h3>
+      <h6 className='ptransript' id='content'>{transcript}</h6>
+      <button onClick={SpeechRecognition.startListening} className="startbut">Start</button>&nbsp;&nbsp;
+      <button onClick={SpeechRecognition.stopListening} className="startbut">Stop</button>&nbsp;&nbsp;
+      <button onClick={resetTranscript} className="startbut">Reset</button>&nbsp;&nbsp;</center>
+      <h3><pre>{response}</pre></h3>
+    </div>
+  );
+};
+export default App;
